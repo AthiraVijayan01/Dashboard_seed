eea647e42a375e964a151baff8aa9442161e11f0
athira vijayan <athirapunnekkad@gmail.com>
2025-03-23 13:19:26 +0530

    first commit

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -12,7 +12,6 @@ const Header = () => {
                 userLoggedIn
                     ?
                     <>
-                        <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }} className="px-4 py-2 text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300 ease-in-out">Logout  </button>
                     </>
                     :
                     <>
